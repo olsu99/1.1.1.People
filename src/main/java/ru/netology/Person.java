bb56bb065c887d81e3e422e5a79dd20d014f00e3
olsu99 <alsou-061999@mail.ru>
2023-03-12 23:45:41 +0300

    first commit

---
+++ src/main/java/ru/netology/Person.java
@@ -0,0 +1,78 @@
+package ru.netology;
+
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(String name, String surname, int age, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+
+    public boolean hasAge() {
+        return age > 0;
+    }
+
+    public boolean hasAddress() {
+        return !address.isEmpty();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        this.age++;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        PersonBuilder childBuilder = new PersonBuilder();
+        childBuilder.setSurname(getSurname());
+        childBuilder.setAddress(getAddress());
+        return childBuilder;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Имя: %s%nФамилия: %s%nВозраст: %s%nАдрес: %s%n", name, surname, hasAge() ? age : "не определён", hasAddress() ? address : "не определён");
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, address);
+    }
+}
